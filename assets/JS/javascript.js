bb5761b643108f08210220d7f0e32ea433cb37ce
John Netzel <johnrnetzel@gmail.com>
2022-04-01 14:20:28 -0800

    display word on screen in masked form when game starts

--- assets/JS/javascript.js
+++ assets/JS/javascript.js
@@ -1,9 +1,15 @@
 var startButton = document.querySelector("#start-game");
+var wordContent = document.querySelector("#word-content");
 
 const wordBank = ["fuzzy", "guitar", "camera", "computer", "keyboard", "mouse", "microphone", "monitor", "coffee", "tea", "water", "sandwich", "pen", "pencil", "telephone"];
+var selectedWord;
+var selectedWordMasked;
 
 startButton.addEventListener("click", function() {
-    console.log(selectGameWord());
+    selectedWord = selectGameWord();
+    maskGameWord();
+    wordContent.textContent = selectedWordMasked;
+    console.log(selectedWord);
 });
 
 // Selects a word at random from the word bank
@@ -13,4 +19,13 @@ function selectGameWord()
     index = index * wordBank.length;
     index = Math.floor(index);
     return wordBank[index];
+}
+
+function maskGameWord()
+{
+    selectedWordMasked = "";
+    for(var i = 0; i < selectedWord.length; i++)
+    {
+        selectedWordMasked += "_";
+    }
 }
