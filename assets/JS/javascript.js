d68d2eebfa59e9d180049cd6d60e1e92af0373e0
John Netzel <johnrnetzel@gmail.com>
2022-04-01 13:54:27 -0800

    Add word bank and random selector method from word bank

--- assets/JS/javascript.js
+++ assets/JS/javascript.js
@@ -1,5 +1,16 @@
 var startButton = document.querySelector("#start-game");
 
+const wordBank = ["fuzzy", "guitar", "camera", "computer", "keyboard", "mouse", "microphone", "monitor", "coffee", "tea", "water", "sandwich", "pen", "pencil", "telephone"];
+
 startButton.addEventListener("click", function() {
-    console.log("Start game");
-})
+    console.log(selectGameWord());
+});
+
+// Selects a word at random from the word bank
+function selectGameWord()
+{
+    var index = Math.random();
+    index = index * wordBank.length;
+    index = Math.floor(index);
+    return wordBank[index];
+}
